d6b2a77a6ae7b90d4346c36c691420c8728cfdd8
Henning Myhrvold <henning.myhrvold@gmail.com>
2024-09-29 13:06:00 +0200

    Troubleshooting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'henningmyhrvold', // Usually your GitHub org/user name.
   projectName: 'henningmyhrvold.github.io', // Usually your repo name.
-  deploymentBranch: 'deployment',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
