07213b141a67d53d158e19258f78f174a35f2eda
henning.myhrvold@gmail.com <henning@Hennings-MBP.lan>
2024-09-29 12:55:52 +0200

    GitHub pages deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/atom.svg',
 
   // Set the production url of your site here
-  url: 'https://henningmyhrvold.github.io/',
+  url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/henningmyhrvold.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'henningmyhrvold', // Usually your GitHub org/user name.
-  projectName: 'https://henningmyhrvold.github.io/', // Usually your repo name.
+  projectName: 'henningmyhrvold.github.io', // Usually your repo name.
   deploymentBranch: 'deployment',
 
   onBrokenLinks: 'warn',
